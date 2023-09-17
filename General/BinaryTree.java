4a66188a93e27e5240ba23c2d6b033b8279aefe1
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-09-17 23:49:20 +0530

    Create BinaryTree.java

---
+++ General/BinaryTree.java
@@ -0,0 +1,69 @@
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+public class BinaryTree {
+    Node root;
+
+    public BinaryTree() {
+        root = null;
+    }
+
+    // Function to insert a new node into the binary tree
+    public void insert(int data) {
+        root = insertRec(root, data);
+    }
+
+    private Node insertRec(Node root, int data) {
+        if (root == null) {
+            root = new Node(data);
+            return root;
+        }
+
+        if (data < root.data) {
+            root.left = insertRec(root.left, data);
+        } else if (data > root.data) {
+            root.right = insertRec(root.right, data);
+        }
+
+        return root;
+    }
+
+    // Function to perform an in-order traversal of the binary tree
+    public void inorderTraversal() {
+        inorderRec(root);
+    }
+
+    private void inorderRec(Node root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.print(root.data + " ");
+            inorderRec(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+
+        // Insert nodes into the binary tree
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.insert(80);
+
+        // Perform in-order traversal to print the nodes
+        System.out.println("In-order traversal of the binary tree:");
+        tree.inorderTraversal();
+    }
+}
