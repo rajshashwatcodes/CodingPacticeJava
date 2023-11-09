2047c2a6e280836cf0a5dbfa9a6c5e92675370e5
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-11-09 23:00:54 +0530

    Create PalindromeChecker.java

---
+++ General/PalindromeChecker.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (isPalindrome(number)) {
+            System.out.println(number + " is a palindrome.");
+        } else {
+            System.out.println(number + " is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isPalindrome(int num) {
+        int originalNumber = num;
+        int reversedNumber = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            num /= 10;
+        }
+
+        return originalNumber == reversedNumber;
+    }
+}
