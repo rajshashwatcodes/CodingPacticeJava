6557f910a31e016d9500d9e14736db010a8c691b
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2024-01-05 23:06:58 +0530

    Create SudokuSolver.java

---
+++ General/SudokuSolver.java
@@ -0,0 +1,88 @@
+public class SudokuSolver {
+
+    public static void main(String[] args) {
+        int[][] sudokuGrid = {
+                {5, 3, 0, 0, 7, 0, 0, 0, 0},
+                {6, 0, 0, 1, 9, 5, 0, 0, 0},
+                {0, 9, 8, 0, 0, 0, 0, 6, 0},
+                {8, 0, 0, 0, 6, 0, 0, 0, 3},
+                {4, 0, 0, 8, 0, 3, 0, 0, 1},
+                {7, 0, 0, 0, 2, 0, 0, 0, 6},
+                {0, 6, 0, 0, 0, 0, 2, 8, 0},
+                {0, 0, 0, 4, 1, 9, 0, 0, 5},
+                {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        if (solveSudoku(sudokuGrid)) {
+            printSudoku(sudokuGrid);
+        } else {
+            System.out.println("No solution exists.");
+        }
+    }
+
+    private static boolean solveSudoku(int[][] grid) {
+        int[] emptyCell = findEmptyCell(grid);
+        if (emptyCell == null) {
+            return true; // Puzzle solved successfully
+        }
+
+        int row = emptyCell[0];
+        int col = emptyCell[1];
+
+        for (int num = 1; num <= 9; num++) {
+            if (isSafe(grid, row, col, num)) {
+                grid[row][col] = num;
+
+                if (solveSudoku(grid)) {
+                    return true;
+                }
+
+                grid[row][col] = 0; // If placing num at (row, col) doesn't lead to a solution, backtrack.
+            }
+        }
+
+        return false; // No valid number can be placed in this cell
+    }
+
+    private static boolean isSafe(int[][] grid, int row, int col, int num) {
+        // Check if the number is not present in the current row and column
+        for (int x = 0; x < 9; x++) {
+            if (grid[row][x] == num || grid[x][col] == num) {
+                return false;
+            }
+        }
+
+        // Check if the number is not present in the 3x3 grid
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (grid[i + startRow][j + startCol] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true; // The number is safe to be placed in this cell
+    }
+
+    private static int[] findEmptyCell(int[][] grid) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (grid[i][j] == 0) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null; // No empty cell found
+    }
+
+    private static void printSudoku(int[][] grid) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
