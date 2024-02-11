2c5427fac27ab22b80a1e7a5cdc834d70ca4eea4
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2024-02-11 14:53:40 +0530

    Create FloydWarshall.java

---
+++ General/FloydWarshall.java
@@ -0,0 +1,52 @@
+public class FloydWarshall {
+    static final int INF = Integer.MAX_VALUE; // Represents infinity
+    
+    // Function to perform Floyd-Warshall algorithm
+    public static void floydWarshall(int[][] graph, int V) {
+        int[][] dist = new int[V][V];
+        
+        // Initialize distances to the given graph
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                dist[i][j] = graph[i][j];
+            }
+        }
+        
+        // Main algorithm
+        for (int k = 0; k < V; k++) {
+            for (int i = 0; i < V; i++) {
+                for (int j = 0; j < V; j++) {
+                    // If vertex k is on the shortest path from i to j, then update the value of dist[i][j]
+                    if (dist[i][k] != INF && dist[k][j] != INF && dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+        
+        // Print the shortest distances
+        System.out.println("Shortest distances between every pair of vertices:");
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][j] == INF) {
+                    System.out.print("INF ");
+                } else {
+                    System.out.print(dist[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void main(String[] args) {
+        int[][] graph = {
+            {0, 5, INF, 10},
+            {INF, 0, 3, INF},
+            {INF, INF, 0, 1},
+            {INF, INF, INF, 0}
+        };
+        int V = 4; // Number of vertices in the graph
+        
+        floydWarshall(graph, V);
+    }
+}
