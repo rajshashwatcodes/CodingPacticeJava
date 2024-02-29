208794173163078da2b458720ef2f33a3ba66d63
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2024-02-29 23:04:57 +0530

    Create StringConverstion.java

--- General/StringConverstion.java
+++ General/StringConverstion.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class StringConvertion {
+public class StringConversion {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
