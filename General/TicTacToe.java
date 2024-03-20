bcdfb4f61a62019c3fc7140c9b1feaa22cc8a620
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2024-03-20 15:13:04 +0530

    Create TicTacToe.java

---
+++ General/TicTacToe.java
@@ -0,0 +1,93 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+    private static char[][] board = new char[3][3];
+    private static char currentPlayer = 'X';
+
+    public static void main(String[] args) {
+        initializeBoard();
+        displayBoard();
+
+        boolean gameOver = false;
+        while (!gameOver) {
+            int[] move = getPlayerMove();
+            int row = move[0];
+            int col = move[1];
+
+            if (isValidMove(row, col)) {
+                board[row][col] = currentPlayer;
+                displayBoard();
+                if (checkWinner(row, col)) {
+                    System.out.println("Player " + currentPlayer + " wins!");
+                    gameOver = true;
+                } else if (isBoardFull()) {
+                    System.out.println("It's a draw!");
+                    gameOver = true;
+                } else {
+                    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+                }
+            } else {
+                System.out.println("Invalid move! Please try again.");
+            }
+        }
+    }
+
+    private static void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+    }
+
+    private static void displayBoard() {
+        System.out.println("  0 1 2");
+        for (int i = 0; i < 3; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static int[] getPlayerMove() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Player " + currentPlayer + ", enter your move (row and column): ");
+        int row = scanner.nextInt();
+        int col = scanner.nextInt();
+        return new int[]{row, col};
+    }
+
+    private static boolean isValidMove(int row, int col) {
+        return row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == '-';
+    }
+
+    private static boolean checkWinner(int row, int col) {
+        // Check row
+        if (board[row][0] == currentPlayer && board[row][1] == currentPlayer && board[row][2] == currentPlayer) {
+            return true;
+        }
+        // Check column
+        if (board[0][col] == currentPlayer && board[1][col] == currentPlayer && board[2][col] == currentPlayer) {
+            return true;
+        }
+        // Check diagonals
+        if ((board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) ||
+                (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '-') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
