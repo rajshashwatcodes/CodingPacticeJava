acc35e98edd2b3dc9a5330bf191da5820b33f55d
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2024-04-19 23:55:23 +0530

    Create LeapYearChecker.java

---
+++ General/LeapYearChecker.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class LeapYearChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+
+        if (isLeapYear(year)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isLeapYear(int year) {
+        // Leap year conditions:
+        // 1. Year is evenly divisible by 4
+        // 2. If year is divisible by 100, it must also be divisible by 400
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                return year % 400 == 0;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
